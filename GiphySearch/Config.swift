3aab5bc95d3528aa2860d0eca439bb891e508f94
Vladimir Kelin <vK@macbook-pro.local>
2016-06-24 09:18:08 +0300

    Initial commit.

---
+++ GiphySearch/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  GiphySearch
+//
+//  Created by Vladimir Kelin on 6/24/16.
+//  Copyright © 2016 Vladimir Kelin. All rights reserved.
+//
+
+import Foundation
+
+
+let kGiphyAPIKey: String = "dc6zaTOxFJmzC"
+
+let kGiphyBaseURLStr: String = "http://api.giphy.com"
---
+++ GiphySearch/UI/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  GiphySearch
+//
+//  Created by Vladimir Kelin on 6/24/16.
+//  Copyright © 2016 Vladimir Kelin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainViewController: UIViewController
+{
+    
+}
